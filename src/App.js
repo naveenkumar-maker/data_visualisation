3179334c52c6a91f68ee9f6292a1587bb6780e59
paapugaani naveenkumar <navivinu9493@gmail.com>
2020-11-10 09:25:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Barchart from './Components/Barchart';
+import Barchart2 from './Components/Barchart2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chart">
+     <h1>MS Dhoni visualisation portal</h1>
+     <Barchart/>
+     <Barchart2/>
+     <h3>the graphs shows the visualisation of batting and bowling average of MS Dhoni sports career</h3>
+    </div>
     </div>
   );
 }
